ce4a916a406c935172ac0f2dc7620b909c0ef197
wbhydra <md3003663@gmail.com>
2025-07-20 00:33:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,97 @@
 async function getAudioList() {
-  const url = `https://raw.githubusercontent.com/wbhydra/audio/refs/heads/main/Water%20Drop%20Sound%20Effect%20-%20Royalty%20Free_01.mp3
-`;
+  const url = `https://singles-son-typical-spread.trycloudflare.com/audioslist`;
 
   try {
-    // const response = await fetch(url);
-    // const data = await response.json();
-
-    // data.forEach((el) => {
-    //   let gaana = document.createElement(`li`);
-    //   gaana.textContent = el.name;
-    //   document.querySelector("ol").appendChild(gaana);
-    // });
-    document.querySelector(".play").addEventListener("click", function () {
-      const audio = document.createElement(`audio`);
-      audio.src = `https://raw.githubusercontent.com/wbhydra/audio/refs/heads/main/Water%20Drop%20Sound%20Effect%20-%20Royalty%20Free_01.mp3`;
-      audio.controls = true;
-      document.body.appendChild(audio);
+    const response = await fetch(url);
+    const data = await response.json();
+    let b = data.files.length - 1 ;
+    
+    data.files.forEach((el) => {
+      let gaana = document.createElement(`li`);
+      gaana.textContent = el.name;
+      document.querySelector("ol").appendChild(gaana);
+    });
+    let audio;
+    let a = 0;
+    audio = document.createElement(`audio`);
+    audio.src = data.files[a].url;
+    document.getElementById("song-name").innerText = data.files[a].name;
+    document.getElementById("song-name-copy").innerText = data.files[a].name;
+    document.body.appendChild(audio);
+    document.querySelector(".fa-play").addEventListener("click", function () {
+      document.querySelector(".fa-play").style.display = "none";
+      document.querySelector(".fa-pause").style.display = "flex";
+      document.getElementById("song-name").innerText = data.files[a].name;
       audio
         .play()
         .then(() => console.log("Audio is playing"))
         .catch((err) => console.error("Play error:", err));
-
-      console.log(audio.src);
-      console.log(`btn clicked`);
     });
+
+    document.querySelector(".fa-pause").addEventListener("click", function () {
+      document.querySelector(".fa-play").style.display = "flex";
+      document.querySelector(".fa-pause").style.display = "none";
+      if (audio) {
+        audio.pause();
+      }
+    });
+
+    document
+      .querySelector(".fa-forward")
+      .addEventListener("click", function () {
+        if (a < data.files.length - 1) {
+          a += 1;
+
+          audio.src = data.files[a].url;
+          document.getElementById("song-name").innerText = data.files[a].name;
+          audio
+            .play()
+            .then(() => console.log("Audio is playing"))
+            .catch((err) => console.error("Play error:", err));
+          document.querySelector(".fa-play").style.display = "none";
+          document.querySelector(".fa-pause").style.display = "flex";
+        } else {
+          a = 0;
+          audio.src = data.files[a].url;
+          document.getElementById("song-name").innerText = data.files[a].name;
+          audio
+            .play()
+            .then(() => console.log("Audio is playing"))
+            .catch((err) => console.error("Play error:", err));
+          document.querySelector(".fa-play").style.display = "none";
+          document.querySelector(".fa-pause").style.display = "flex";
+        }
+      });
+    document
+      .querySelector(".fa-backward")
+      .addEventListener("click", function () {
+        if (a > 0) {
+          a -= 1;
+
+          audio.src = data.files[a].url;
+          document.getElementById("song-name").innerText = data.files[a].name;
+          audio
+            .play()
+            .then(() => console.log("Audio is playing"))
+            .catch((err) => console.error("Play error:", err));
+        } else if ((a = 0)) {
+          audio.src = data.files[a].url;
+          document.getElementById("song-name").innerText = data.files[a].name;
+          audio
+            .play()
+            .then(() => console.log("Audio is playing"))
+            .catch((err) => console.error("Play error:", err));
+        } else {
+          a = b;
+          audio.src = data.files[a].url;
+          document.getElementById("song-name").innerText = data.files[a].name;
+          audio.play();
+        }
+      });
   } catch (err) {
     console.error("Failed to fetch audio list:", err);
   }
+  
 }
 
 getAudioList();
