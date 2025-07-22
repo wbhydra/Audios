cea12629be5cca59f838943cefe497acda381260
wbhydra <md3003663@gmail.com>
2025-07-23 00:52:33 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
 async function getAudioList() {
-  const url = `https://singles-son-typical-spread.trycloudflare.com/audioslist`;
+  const url = `https://aaron-blackjack-doubt-libraries.trycloudflare.com/audioslist`;
+  const newUrl = `https://aaron-blackjack-doubt-libraries.trycloudflare.com`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    let b = data.files.length - 1 ;
     
+    let b = data.files.length - 1;
+
     data.files.forEach((el) => {
       let gaana = document.createElement(`li`);
       gaana.textContent = el.name;
+      gaana.dataset.name = el.name;
       document.querySelector("ol").appendChild(gaana);
     });
     let audio;
     let a = 0;
     audio = document.createElement(`audio`);
-    audio.src = data.files[a].url;
-    document.getElementById("song-name").innerText = data.files[a].name;
-    document.getElementById("song-name-copy").innerText = data.files[a].name;
+    audio.src = data.files[a].url.replace(
+    "https://singles-son-typical-spread.trycloudflare.com",
+    newUrl
+);
     document.body.appendChild(audio);
-    document.querySelector(".fa-play").addEventListener("click", function () {
-      document.querySelector(".fa-play").style.display = "none";
-      document.querySelector(".fa-pause").style.display = "flex";
+    console.log(data.files[a]);
+    console.log(audio.src);
+   
+
+
+
+    if (!audio) {
       document.getElementById("song-name").innerText = data.files[a].name;
-      audio
-        .play()
-        .then(() => console.log("Audio is playing"))
-        .catch((err) => console.error("Play error:", err));
+      document.getElementById("song-name-copy").innerText = data.files[a].name;
+    }
+
+    document.querySelector(".fa-play").addEventListener("click", function () {
+      if(audio.paused){
+        audio.play();
+         document.querySelector(".fa-play").style.display = "none";
+      document.querySelector(".fa-pause").style.display = "flex";
+      }else{
+        play();
+      }
     });
 
     document.querySelector(".fa-pause").addEventListener("click", function () {
@@ -41,25 +56,10 @@ async function getAudioList() {
       .addEventListener("click", function () {
         if (a < data.files.length - 1) {
           a += 1;
-
-          audio.src = data.files[a].url;
-          document.getElementById("song-name").innerText = data.files[a].name;
-          audio
-            .play()
-            .then(() => console.log("Audio is playing"))
-            .catch((err) => console.error("Play error:", err));
-          document.querySelector(".fa-play").style.display = "none";
-          document.querySelector(".fa-pause").style.display = "flex";
+          play();
         } else {
           a = 0;
-          audio.src = data.files[a].url;
-          document.getElementById("song-name").innerText = data.files[a].name;
-          audio
-            .play()
-            .then(() => console.log("Audio is playing"))
-            .catch((err) => console.error("Play error:", err));
-          document.querySelector(".fa-play").style.display = "none";
-          document.querySelector(".fa-pause").style.display = "flex";
+          play();
         }
       });
     document
@@ -67,31 +67,77 @@ async function getAudioList() {
       .addEventListener("click", function () {
         if (a > 0) {
           a -= 1;
-
-          audio.src = data.files[a].url;
-          document.getElementById("song-name").innerText = data.files[a].name;
-          audio
-            .play()
-            .then(() => console.log("Audio is playing"))
-            .catch((err) => console.error("Play error:", err));
+          play();
         } else if ((a = 0)) {
-          audio.src = data.files[a].url;
-          document.getElementById("song-name").innerText = data.files[a].name;
-          audio
-            .play()
-            .then(() => console.log("Audio is playing"))
-            .catch((err) => console.error("Play error:", err));
+          play();
         } else {
           a = b;
-          audio.src = data.files[a].url;
-          document.getElementById("song-name").innerText = data.files[a].name;
-          audio.play();
+          play();
         }
       });
+
+    function audioName() {
+      let songList = document.getElementById("song-list");
+      songList.addEventListener("click", (e) => {
+        try {
+          let hero = data.files;
+
+          let clickedSong = hero.find(
+            (song) => song.name === e.target.dataset.name
+          );
+          document.getElementById("song-name").innerText = clickedSong.name;
+          audio.src = clickedSong.url.replace(
+    "https://singles-son-typical-spread.trycloudflare.com",
+    newUrl
+);
+          audio.play();
+
+          document.querySelector(".fa-play").style.display = "none";
+          document.querySelector(".fa-pause").style.display = "flex";
+        } catch (err) {
+          console.error("Failed to fetch  list:", err);
+        }
+      });
+    }
+    audioName();
+    function play() {
+      let currentSrc = data.files[a].url.replace("https://singles-son-typical-spread.trycloudflare.com",
+    newUrl);
+      if(audio.src !== currentSrc)
+  audio.src = data.files[a].url.replace(
+    "https://singles-son-typical-spread.trycloudflare.com",
+    newUrl
+);
+  document.getElementById("song-name").innerText = data.files[a].name;
+  audio.play();
+
+  document.querySelector(".fa-play").style.display = "none";
+  document.querySelector(".fa-pause").style.display = "flex";
+};
+
+function formatTime(timeInSeconds) {
+            let minutes = Math.floor(timeInSeconds / 60);
+            let seconds = Math.floor(timeInSeconds % 60);
+            return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+        }
+        let duration_song = document.querySelector(".duration");
+        
+         audio.addEventListener('loadedmetadata', () => {
+    
+    duration_song.innerText=formatTime(audio.duration);
+});
+        
+          audio.addEventListener('timeupdate', () => {
+            let remaining = audio.duration - audio.currentTime;
+            duration_song.innerText = formatTime(remaining);
+    
+});
+
+
   } catch (err) {
     console.error("Failed to fetch audio list:", err);
   }
-  
 }
 
 getAudioList();
+
